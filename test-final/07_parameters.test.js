a74e2914ed9f8b335766a019bafd5b09a94f2d5c
jebli mohamed <jeblister@gmail.com>
2015-11-20 15:05:49 +0000

    parameters

--- test-final/07_parameters.test.js
+++ test-final/07_parameters.test.js
@@ -1,4 +1,51 @@
-import {expect} from 'chai';
+import {expect, assert} from 'chai';
+
+// 57: Default parameters - basics
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('default parameters make function parameters more flexible', () => {
+
+  it('define it using an assignment to the parameter `function(param=1){}`', function() {
+    let number = (int=0) => int;
+
+    assert.equal(number(), 0);
+  });
+
+  it('it is used when undefined is passed', function() {
+    let number = (int = 23) => int;
+    const param = 23;
+
+    assert.equal(number(param), 23);
+  });
+
+  it('it is not used when a value is given', function() {
+    function xhr(method = 'GET') {
+      return method;
+    }
+
+    assert.equal(xhr('POST'), 'POST');
+  });
+
+  it('it is evaluated at run time', function() {
+    let defaultValue = 42;
+    function xhr(method = `value: ${defaultValue}`) {
+      return method;
+    }
+
+    assert.equal(xhr(), 'value: 42');
+    defaultValue = 23;
+  });
+
+  it('it can also be a function', function() {
+    let defaultValue = function (){};
+    function fn(value = defaultValue()) {
+      return value;
+    }
+
+    assert.equal(fn(), defaultValue());
+  });
+
+});
 
 describe('Default Values', () => {
   it('can be triggered when the incoming argument is undefined', () => {
@@ -8,9 +55,10 @@ describe('Default Values', () => {
     }
 
     expect(test('Aaron')).to.equal('Aaron');
-    expect(test()).to.equal('Mercury');
     expect(test(undefined)).to.equal('Mercury');
     expect(test(null)).to.equal(null);
+    expect(test()).to.equal('Mercury');
+
   });
 
   it(`aren't included in arguments`, () => {
@@ -20,7 +68,9 @@ describe('Default Values', () => {
     }
 
     expect(test('Aaron')).to.equal(1);
+    expect(test(null)).to.equal(1);
     expect(test()).to.equal(0);
+
   });
 
   it('can trigger a function call', () => {
@@ -38,8 +88,9 @@ describe('Default Values', () => {
     expect(triggerCount).to.equal(0);
     expect(test('Aaron')).to.equal('Aaron');
     expect(test()).to.equal('Mercury');
-    expect(test(undefined)).to.equal('Mercury');
-    expect(triggerCount).to.equal(2);
+    expect(test(null)).to.equal(null);
+    expect(triggerCount).to.equal(1);
+
   });
 
 });
@@ -57,6 +108,7 @@ describe('Rest Parameters', () => {
     expect(resty(1, 2).length).to.equal(0);
     expect(resty(1, 2, 3).length).to.equal(1);
     expect(resty(1, 2, 3, undefined, 5, undefined, 7, undefined, 9, 10).length).to.equal(8);
+
   });
 
   it('has a different length than `arguments`', () => {
@@ -65,11 +117,11 @@ describe('Rest Parameters', () => {
       return others.length == arguments.length;
     }
 
-    expect(resty()).to.be.true;
-    expect(resty(1)).to.be.false;
-    expect(resty(1, 2)).to.be.false;
-    expect(resty(1, 2, 3)).to.be.false;
-    expect(resty(1, 2, 3, undefined, 5, undefined, 7, undefined, 9, 10)).to.be.false;
+    expect(resty()).to.equal(true);
+    expect(resty(1)).to.equal(false);
+    expect(resty(1, 2)).to.equal(false);
+    expect(resty(1, 2, 3)).to.equal(false);
+    expect(resty(1, 2, 3, undefined, 5, undefined, 7, undefined, 9, 10)).to.equal(false);
   });
 
   it('is an actual array, unlike arguments', () => {
@@ -85,28 +137,24 @@ describe('Rest Parameters', () => {
     var args = argy(1, 2, 3);
     var rests = resty(1, 2, 3);
 
-    expect(args.__proto__ == rests.__proto__).to.be.false;
-    expect(args.splice).to.be.undefined;
+    expect(args.__proto__ == rests.__proto__).to.equal(false);
+    expect(args.splice).to.equal(undefined);
 
     expect(rests.__proto__).to.equal(Array.prototype);
     expect(rests.splice).to.exist;
     expect(rests.splice).to.equal(Array.prototype.splice);
+
   });
 
 
   describe('EXTRA CREDIT', ()=> {
-
     it('it can default all arguments, optionally', () => {
 
       // Modify the method signature of `myFunction` to allow for all args to be optional
 
-      const defaultObj = {
-        name: 'Aaron',
-        age: 35,
-        favoriteBand: 'Queen'
-      };
+      // function myFunc(requiredArg, optionalArg = 'defaultValue') {// do stuff }
 
-      function myFunction({name = 'Aaron', age = 35, favoriteBand = 'Queen'} = defaultObj) {
+      function myFunction(name="", age=25, favoriteBand ="Taylor Swift") {
         expect(name).to.exist;
         expect(age).to.exist;
         expect(favoriteBand).to.exist;
